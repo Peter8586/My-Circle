4f0840e2bb4782631ef5f8114da8805335f23a30
Peter8586 <peterpineda@Peters-MacBook-Pro-2.local>
2017-06-22 06:53:40 -0700

    Completed setting up Firebse integration

---
+++ mycircle/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mycircle
+//
+//  Created by Peter Pineda on 6/22/17.
+//  Copyright © 2017 Peter Pineda. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
